dcb42871fc08920be588751096c00245fc218a62
Mahajan <nmahajan24@dxc.com>
2020-03-02 18:03:00 +0530

    java commit

---
+++ HibernateMaven/src/main/java/com/student/services/StudentService.java
@@ -0,0 +1,26 @@
+package com.student.services;
+
+public class StudentService {
+	private String name;
+	private String age;
+	private String rollNo;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(String rollNo) {
+		this.rollNo = rollNo;
+	}
+	
+}
